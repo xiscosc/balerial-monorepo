66425e10c654957ecda792830361db12b94488c6
Xisco Sastre Cabot <xiscosastre@gmail.com>
2025-10-11 17:07:14 +0200

    refactor: optimize order retrieval and enhance loading experience with progress indicator

--- apps/marcos/src/routes/(app)/(main)/orders/[id]/print/+page.svelte
+++ apps/marcos/src/routes/(app)/(main)/orders/[id]/print/+page.svelte
@@ -4,25 +4,45 @@
 	import OrderPrint from '@/components/business-related/order-detail/OrderPrint.svelte';
 	import Button from '@/components/generic/button/Button.svelte';
 	import { IconType } from '@/components/generic/icon/icon.enum';
+	import { getGlobalProfiler } from '@/state/profiler/profiler.state';
+	import Box from '@/components/generic/Box.svelte';
+	import ProgressBar from '@/components/generic/ProgressBar.svelte';
 	interface Props {
 		data: PageData;
 	}
 
 	let { data }: Props = $props();
+	const fullOrderPromise = getGlobalProfiler().measure(data.fullOrder);
 	identifyUser(data.user, data.envName);
 </script>
 
-{#snippet actionButtons()}
-	<Button text="Volver atrás" icon={IconType.LEFT} onClick={() => window.history.back()}></Button>
-	<Button
-		text="Ir al pedido"
-		icon={IconType.ORDER_DEFAULT}
-		link={`/orders/${data.fullOrder.order.id}`}
-	></Button>
-{/snippet}
-
-<div class="flex w-full items-center justify-center print:block">
-	<div class="w-fit print:w-auto">
-		<OrderPrint fullOrder={data.fullOrder} print buttons={actionButtons}></OrderPrint>
-	</div>
-</div>
+{#await fullOrderPromise}
+	<Box>
+		<ProgressBar text="Generando resguardo..."></ProgressBar>
+	</Box>
+{:then fullOrder}
+	{#if fullOrder}
+		{#snippet actionButtons()}
+			<Button text="Volver atrás" icon={IconType.LEFT} onClick={() => window.history.back()}
+			></Button>
+			<Button
+				text="Ir al pedido"
+				icon={IconType.ORDER_DEFAULT}
+				link={`/orders/${fullOrder.order.id}`}
+			></Button>
+		{/snippet}
+		<div class="flex w-full items-center justify-center print:block">
+			<div class="w-fit print:w-auto">
+				<OrderPrint {fullOrder} print buttons={actionButtons}></OrderPrint>
+			</div>
+		</div>
+	{:else}
+		<div class="flex flex-col gap-2">
+			<Box icon={IconType.NOT_FOUND} title="Error">
+				<p class="text-center text-lg">No se ha encontrado el pedido</p>
+			</Box>
+			<Button text="Volver atrás" icon={IconType.LEFT} onClick={() => window.history.back()}
+			></Button>
+		</div>
+	{/if}
+{/await}
--- apps/marcos/src/routes/(public)/+layout.svelte
+++ apps/marcos/src/routes/(public)/+layout.svelte
@@ -16,13 +16,3 @@
 </svelte:head>
 
 {@render children?.()}
-
-<style>
-	@media print {
-		:global(body),
-		:global(html) {
-			margin: 0 !important;
-			padding: 0 !important;
-		}
-	}
-</style>
