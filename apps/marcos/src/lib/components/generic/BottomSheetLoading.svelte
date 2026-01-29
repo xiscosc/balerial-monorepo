5cc0677b6df22c8be0037e3b9e8e64389ca2950d
Xisco Sastre Cabot <xiscosastre@gmail.com>
2026-01-29 11:43:10 +0100

    revert jan update

--- apps/marcos/src/lib/components/generic/BottomSheetLoading.svelte
+++ apps/marcos/src/lib/components/generic/BottomSheetLoading.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-	import MaterialLoading from '@/components/generic/MaterialLoading.svelte';
+	import Icon from '@/components/generic/icon/Icon.svelte';
+	import { IconSize, IconType } from '@/components/generic/icon/icon.enum';
 </script>
 
 <div class="py-6">
 	<div class="flex w-full flex-col items-center justify-center gap-2">
-		<MaterialLoading />
+		<div class="animate-spin">
+			<Icon size={IconSize.XXXL} type={IconType.LOADING_BALL} />
+		</div>
 		<span class="text-md text-gray-600">Aplicando cambios...</span>
 	</div>
 </div>
--- apps/marcos/src/lib/components/generic/MaterialLoading.svelte
+++
@@ -1,8 +0,0 @@
-<m3e-loading-indicator variant="contained" class="loading"></m3e-loading-indicator>
-
-<style>
-	.loading {
-		--m3e-loading-indicator-contained-active-indicator-color: var(--color-gray-700);
-		--m3e-loading-indicator-contained-container-color: var(--color-gray-100);
-	}
-</style>
