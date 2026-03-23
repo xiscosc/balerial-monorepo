9ebc3ef4aa41de58f3be6e2a69da2bb53ee91f5f
Xisco Sastre Cabot <xiscosastre@gmail.com>
2026-03-23 10:21:19 +0100

    fix: enhance authentication check and improve session tracking

--- apps/marcos/src/lib/server/shared/auth/user.ts
+++ apps/marcos/src/lib/server/shared/auth/user.ts
@@ -2,6 +2,7 @@ import { type Handle } from '@sveltejs/kit';
 import { AuthService } from '$lib/server/service/auth.service';
 import type { CustomSession } from '$lib/type/api.type';
 import { ServiceFactory } from '@marcsimolduressonsardina/core/service';
+import { ServerTracking } from '$lib/server/shared/tracking';
 
 export const getUserHandle: Handle = async ({ event, resolve }) => {
 	const session = await event.locals.auth();
@@ -11,6 +12,11 @@ export const getUserHandle: Handle = async ({ event, resolve }) => {
 		const config = AuthService.generateConfiguration(user);
 		event.locals.config = config;
 		event.locals.services = ServiceFactory.create(config);
+	} else if (session) {
+		ServerTracking.anonymousEvent('invalid_session', {
+			context: event.locals.trackingContext,
+			properties: { url: event.url.pathname }
+		});
 	}
 	return resolve(event);
 };
